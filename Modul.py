aab4c5aea60ebb565445ff711cdb8830d6c234c2
Simon Grad <simon.grad3@gmail.com>
2021-04-30 19:45:24 +0200

    Dodana funk. nova_igra in kons. STEVILO_POSKUSOV

--- Modul.py
+++ Modul.py
@@ -2,6 +2,7 @@ import rendom
 
 BELA, ČRNA, NEVIDNA = "B", "Č", "N"
 RDEČA, MODRA, ZELENA, ORANŽNA, VIJOLIČNA, RUMENA, ROZA = "R", "M", "Z", "O", "V","Y", "P" 
+STEVILO_POSKUSOV = 6
 
 def premešaj(seznam):
     return random.shuffle(seznam)
@@ -10,8 +11,9 @@ def izloči(črka, seznam):
     return seznam.remove(črka)
 
 class Memo:
-    def __init__(self, geslo):
+    def __init__(self, geslo, level):
         self.geslo = geslo
+        self.level = level
 
     def preveri_vrstico(self, ugibanje):
         output = []
@@ -26,4 +28,31 @@ class Memo:
                 output.append(NEVIDNA)
         return premešaj(output)
     
+    def zmaga(self, ugibanje):
+        return preveri_vrstico(self, ugibanje) == [ČRNA] * 4 
 
+
+def izberi_datoteko(level):
+    if level == 1:
+        return "Kombinacije_level1.txt"
+    elif level == 2:
+        return "Kombinacije_level2.txt"
+    elif level == 3:
+        return "Kombinacije_level3.txt"
+    elif level == 4:
+        return "Kombinacije_level4.txt"
+    elif level == 5:
+        return "Kombinacije_level5.txt"
+    elif level == 6:
+        return "Kombinacije_level6.txt"
+    elif level == 7:
+        return "Kombinacije_level7.txt"
+
+
+def nova_igra(level):
+    datoteka = izberi_datoteko(level)
+    with open(datoteka, encoding="utf8") as dat:
+        možnosti = dat.read().split()
+        geslo = rendom.choice(možnosti)
+    return Memo(geslo, level)
+        
