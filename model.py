2922526992c4d2baed163a504b90444756177b3b
Simon Grad <simon.grad3@gmail.com>
2021-05-06 21:00:14 +0200

    Popravil model- ki preverja vrstico (ČRNI-BELI)

--- model.py
+++ model.py
@@ -8,30 +8,41 @@ def premešaj(seznam):
     return random.shuffle(seznam)
 
 def izloči(črka, seznam):
+    """Iz seznama izloči črko"""
     return seznam.remove(črka)
 
 class Memo:
-    def __init__(self, geslo, level, ugibanja =[]):
+    def __init__(self, geslo, level, ugibanja=None):
         self.geslo = geslo
         self.level = level
-        self.ugibanja = ugibanja
+        self.ugibanja = ugibanja or list()
     
     def __repr__(self):
         return f"Memo({self.geslo}, {self.level}, {self.ugibanja})"
 
     def preveri_vrstico(self, ugibanje):
         self.ugibanja.append(ugibanje)
-        output = []
+        output = list()
+        geslo_za_preverjanje = [i for i in self.geslo]
+        ugibanje_za_preverjanje = [i for i in ugibanje]
+        
+        """Preveri za tiste, ki so na iste barve na istem mestu"""
         for i in range(4):
-            if ugibanje[i] == self.geslo[i]:
+            if ugibanje_za_preverjanje[3-i] == geslo_za_preverjanje[3-i]:
                 output.append(ČRNA)
-                izloči(ugibanje[i], self.geslo)
-            elif ugibanje[i] in self.geslo:
-                output.append(BELA) 
-                izloči(ugibanje[i], self.geslo)
+                geslo_za_preverjanje.pop(3-i)
+                ugibanje_za_preverjanje.pop(3-i)
+
+        """Preveri še ostale"""
+        n = len(ugibanje_za_preverjanje)
+        for i in range(n):
+            if ugibanje_za_preverjanje[n-i-1] in geslo_za_preverjanje:
+                output.append(BELA)
+                izloči(ugibanje_za_preverjanje[n-i-1], geslo_za_preverjanje)
             else:
                 output.append(NEVIDNA)
-        return premešaj(output)
+
+        return output
 
     def št_napak(self):
         return len(self.ugibanja)
