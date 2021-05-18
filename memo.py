9cedf9437cd6bea99df146c205c72d522b498ed8
Simon Grad <simon.grad3@gmail.com>
2021-05-18 16:19:48 +0200

    osnovni spletni vmesnik

---
+++ memo.py
@@ -0,0 +1,24 @@
+import bottle
+import model
+
+@bottle.get("/css/<datoteka>")
+def neki(datoteka):
+    return bottle.static_file(datoteka, root="css")
+
+@bottle.get("/")
+def začetna():
+    return bottle.template("zacetna.html")
+
+@bottle.get("/igra/")
+def pokaži_igro():
+    return bottle.template("baza.html")
+
+@bottle.get("/nastavi_stopnjo/")
+def nastavi_stopnjo():
+    return bottle.template("nastavi_stopnjo.html")
+
+
+
+
+
+bottle.run(reloader=True, debug=True)
